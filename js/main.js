d41369781c5d320fbea566aeb047b777e7f08b87
chill <wiktor.ignasiak2@gmail.com>
2024-03-03 14:33:47 +0100

    03.03 v2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-feather.replace();
+const swiper = new Swiper(".swiper", {
+	effect: "coverflow",
+	speed: 1000,
+	loop: true,
+	grabCursor: true,
+	slidesPerView: 1,
+	FreeMode: {
+		enabled: true,
+	},
+	autoplay: {
+		delay: 2500,
+		pauseOnMouseEnter: true,
+	},
+	breakpoints: {
+		// when window width is >= 320px
+		576: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+	},
+});
