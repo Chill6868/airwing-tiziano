561e8730a0dfc87af6229db69155ff64a2fc996c
chill <wiktor.ignasiak2@gmail.com>
2024-03-10 17:01:27 +0100

    10.03 vbro cooked

--- js/main.js
+++ js/main.js
@@ -22,18 +22,45 @@ const swiper = new Swiper(".swiper", {
 	},
 });
 
+// -------------------------------------------------------------------------------------------------
+
 const navDesktop = document.querySelector(".nav");
 
 const handleNav = () => {
 	if (scrollY >= 100) {
 		navDesktop.style.transform = "translateY(-50px)";
-		navDesktop.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-		navDesktop.style.backdropFilter = "blur(16px)";
+		// navDesktop.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+		// navDesktop.style.backdropFilter = "blur(16px)";
 	} else {
 		navDesktop.style.transform = "translateY(0)";
-		navDesktop.style.backgroundColor = "";
-		navDesktop.style.backdropFilter = "blur(0)";
+		// navDesktop.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+		// navDesktop.style.backdropFilter = "blur(0)";
 	}
 };
 
 document.addEventListener("scroll", handleNav);
+// -------------------------------------------------------------------------------------------------
+
+const video = document.querySelector(".header__video");
+let videos;
+
+if (window.innerWidth < 1200) {
+	videos = ["./videos/header/header1sm.mp4", "./videos/header/header2sm.mp4"];
+} else {
+	videos = ["./videos/header/header1lg.mp4", "./videos/header/header2lg.mp4"];
+}
+
+let activeVideo = 0;
+
+video.src = videos[activeVideo];
+video.play();
+
+video.addEventListener("ended", () => {
+	if (activeVideo == 0) {
+		activeVideo = 1;
+	} else {
+		activeVideo = 0;
+	}
+	video.src = videos[activeVideo];
+	video.play();
+});
