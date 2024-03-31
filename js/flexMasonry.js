ff0570c208fc98f496a2476b4439ac1712d0ba57
chill <wiktor.ignasiak2@gmail.com>
2024-03-31 13:14:33 +0200

    31.03 v8

--- js/flexMasonry.js
+++ js/flexMasonry.js
@@ -1,8 +1,6 @@
 FlexMasonry.init(".gallery__container", {
 	responsive: true,
 	breakpointCols: {
-		"min-width: 1500px": 7,
-		"min-width: 1200px": 6,
 		"min-width: 992px": 5,
 		"min-width: 768px": 4,
 		"min-width: 576px": 3,
