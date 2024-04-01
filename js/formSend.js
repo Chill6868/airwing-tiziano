1d096619444dfc030ab56005b85225b00ba9c225
chill <wiktor.ignasiak2@gmail.com>
2024-04-01 22:19:31 +0200

    1.04 v4 -fix form

--- js/formSend.js
+++ js/formSend.js
@@ -1,36 +1,36 @@
 const form = document.querySelector(".contact__box--form-box");
 
-// form.addEventListener("submit", (e) => {
-// 	e.preventDefault();
-
-// const formHeight = form.clientHeight;
-// form.classList.add("contact__box--form-box--done");
-// form.textContent = "";
-// form.style.height = `${formHeight}px`;
-// form.innerHTML = `
-//  	<div class="contact__form-send-box">
-//  		<i class="fa-solid fa-circle-check contact__form-send-icon"></i>
-//  		<p class="contact__form-send-text">Grazie per averci contattato! Vi risponderemo a breve.</p>
-//  	</div>
-//  `;
-// e.target.submit();
-// });
-
 // find elements
-$(".contact__box--form-box").submit(function (event) {
+$("#staticform").submit(function (event) {
 	event.preventDefault();
 	$.ajax({
 		url: "https://api.staticforms.xyz/submit", // url where to submit the request
 		type: "POST", // type of action POST || GET
 		dataType: "json", // data type
-		data: $(".contact__box--form-box").serialize(), // post data || get data
+		data: $("#staticform").serialize(), // post data || get data
 		success: function (result) {
-			// you can see the result from the console
-			// tab of the developer tools
-			alert(JSON.parse(result));
+			const formHeight = form.clientHeight;
+			form.classList.add("contact__box--form-box--done");
+			form.textContent = "";
+			form.style.height = `${formHeight}px`;
+			form.innerHTML = `
+ 				<div class="contact__form-send-box">
+ 					<i class="fa-solid fa-circle-check contact__form-send-icon"></i>
+ 					<p class="contact__form-send-text">Grazie per averci contattato! Vi risponderemo a breve.</p>
+ 				</div>
+ 			`;
 		},
 		error: function (xhr, resp, text) {
-			alert(xhr, resp, text);
+			const formHeight = form.clientHeight;
+			form.classList.add("contact__box--form-box--done");
+			form.textContent = "";
+			form.style.height = `${formHeight}px`;
+			form.innerHTML = `
+ 				<div class="contact__form-send-box">
+ 					<i class="fa-solid fa-circle-xmark contact__form-send-icon"></i>
+ 					<p class="contact__form-send-text">Si è verificato un errore. Riprovare più tardi. In alternativa, potete contattarci via e-mail o telefono</p>
+ 				</div>
+ 			`;
 		},
 	});
 });
--- js/formSendEn.js
+++ js/formSendEn.js
@@ -1,16 +1,36 @@
 const form = document.querySelector(".contact__box--form-box");
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-
-	const formHeight = form.clientHeight;
-	form.classList.add("contact__box--form-box--done");
-	form.textContent = "";
-	form.style.height = `${formHeight}px`;
-	form.innerHTML = `
-		<div class="contact__form-send-box">
-			<i class="fa-solid fa-circle-check contact__form-send-icon"></i>
-			<p class="contact__form-send-text">Thank you for reaching out! We'll get back to you shortly.</p>
-		</div>
-	`;
+// find elements
+$("#staticform").submit(function (event) {
+	event.preventDefault();
+	$.ajax({
+		url: "https://api.staticforms.xyz/submit", // url where to submit the request
+		type: "POST", // type of action POST || GET
+		dataType: "json", // data type
+		data: $("#staticform").serialize(), // post data || get data
+		success: function (result) {
+			const formHeight = form.clientHeight;
+			form.classList.add("contact__box--form-box--done");
+			form.textContent = "";
+			form.style.height = `${formHeight}px`;
+			form.innerHTML = `
+ 				<div class="contact__form-send-box">
+ 					<i class="fa-solid fa-circle-check contact__form-send-icon"></i>
+ 					<p class="contact__form-send-text">Thank you for reaching out! We'll get back to you shortly.</p>
+ 				</div>
+ 			`;
+		},
+		error: function (xhr, resp, text) {
+			const formHeight = form.clientHeight;
+			form.classList.add("contact__box--form-box--done");
+			form.textContent = "";
+			form.style.height = `${formHeight}px`;
+			form.innerHTML = `
+ 				<div class="contact__form-send-box">
+ 					<i class="fa-solid fa-circle-xmark contact__form-send-icon"></i>
+ 					<p class="contact__form-send-text">An error occurred. Please try again later. Alternatively, you can contact us via email or phone.</p>
+ 				</div>
+ 			`;
+		},
+	});
 });
